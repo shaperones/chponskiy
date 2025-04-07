9e7eee274887799c216d0e83448a588c0d7b032b
shaperones <70793408+shaperones@users.noreply.github.com>
2025-04-07 13:28:40 +0300

    add GlossaryItem and LeaderboardRecord models

--- chponskiy/models.py
+++ chponskiy/models.py
@@ -1,3 +1,22 @@
+import uuid
+from uuid import UUID
 from django.db import models
 
 # Create your models here.
+class GlossaryItem(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    phrase_orig = models.TextField()
+    phrase_translation = models.TextField()
+
+    class Meta:
+        db_table = 'glossary'
+
+
+class LeaderboardRecord(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    username = models.TextField()
+    difficulty = models.TextField()
+    score = models.IntegerField()
+
+    class Meta:
+        db_table = 'leaderboard'
