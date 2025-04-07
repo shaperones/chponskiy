6d7ba32772eb7517b9bf3cd10b4dd3965b4dc13f
shaperones <70793408+shaperones@users.noreply.github.com>
2025-04-07 13:43:44 +0300

    add GlossaryItem and LeaderboardRecord to admin

--- chponskiy/admin.py
+++ chponskiy/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
-
+from chponskiy.models import GlossaryItem, LeaderboardRecord
 # Register your models here.
+
+@admin.register(GlossaryItem)
+class GlossaryItemAdmin(admin.ModelAdmin):
+    search_fields = ('phrase_orig', 'phrase_translation')
+
+
+@admin.register(LeaderboardRecord)
+class LeaderboardRecordAdmin(admin.ModelAdmin):
+    search_fields = ('username', 'score', 'difficulty')
