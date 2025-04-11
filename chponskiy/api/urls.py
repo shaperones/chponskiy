f1f2db84d40dd971c663ced791e69efea73b35d9
shaperones <70793408+shaperones@users.noreply.github.com>
2025-04-11 13:31:34 +0300

    fix linting in urls.py - module level docstrings

--- chponskiy/api/urls.py
+++ chponskiy/api/urls.py
@@ -1,3 +1,5 @@
+"""Configures urls for app's API across all API versions."""
+
 from django.urls import path, include
 
 urlpatterns = [
--- chponskiy/api/v1/urls.py
+++ chponskiy/api/v1/urls.py
@@ -1,3 +1,7 @@
+"""Configures urls across APIv1 of the app.
+Provides the rendering page per difficulty,
+get random question per difficulty, and score uploads"""
+
 from django.urls import path
 
 from chponskiy.api.v1 import views
